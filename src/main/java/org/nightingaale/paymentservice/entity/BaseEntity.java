f15a639f7a7954ae08e171d6e8470586fb0bd09c
Nightingaale <shirounightingaleoff@gmail.com>
2025-09-04 02:34:16 +0300

    feature: add base entity for future entities

---
+++ src/main/java/org/nightingaale/paymentservice/entity/BaseEntity.java
@@ -0,0 +1,30 @@
+package org.nightingaale.paymentservice.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Version;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+public class BaseEntity {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long transactionId;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Version
+    private Long version;
+}
