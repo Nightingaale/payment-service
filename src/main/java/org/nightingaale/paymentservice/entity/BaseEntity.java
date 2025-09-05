a0a9d4a8a1ade58779d1dae8aeed635a6e17baf6
Nightingaale <shirounightingaleoff@gmail.com>
2025-09-05 16:26:46 +0300

    refactor: id annotation

--- src/main/java/org/nightingaale/paymentservice/entity/BaseEntity.java
+++ src/main/java/org/nightingaale/paymentservice/entity/BaseEntity.java
@@ -1,9 +1,6 @@
 package org.nightingaale.paymentservice.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.Version;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -16,6 +13,7 @@ import java.time.LocalDateTime;
 @Setter
 public class BaseEntity {
 
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long transactionId;
 
