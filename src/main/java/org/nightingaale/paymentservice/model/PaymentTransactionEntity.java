a6e903cd65e37138a01d3cdef011050316aa4088
Nightingaale <shirounightingaleoff@gmail.com>
2025-09-09 22:30:53 +0300

    feature: add payment transaction entity

---
+++ src/main/java/org/nightingaale/paymentservice/model/PaymentTransactionEntity.java
@@ -0,0 +1,28 @@
+package org.nightingaale.paymentservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.nightingaale.paymentservice.enums.PaymentTransactionStatus;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "payments")
+public class PaymentTransactionEntity extends BaseEntity {
+
+    @OneToOne
+    @JoinColumn(name = "payment_id", referencedColumnName = "id")
+    private RefundTransactionEntity transactionId;
+
+    @NotNull
+    private PaymentTransactionStatus paymentStatus;
+
+    @NotNull
+    private String currency;
+}
