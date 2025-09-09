7f327cc70a103ee6dc196ca09624a264aef94ecb
Nightingaale <shirounightingaleoff@gmail.com>
2025-09-09 22:31:04 +0300

    feature: add refund transaction entity

---
+++ src/main/java/org/nightingaale/paymentservice/model/RefundTransactionEntity.java
@@ -0,0 +1,34 @@
+package org.nightingaale.paymentservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.nightingaale.paymentservice.enums.PaymentTransactionStatus;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "refunds")
+public class RefundTransactionEntity extends BaseEntity {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long transactionId;
+
+    @NotNull
+    private Long refundedAmount;
+
+    @NotNull
+    private PaymentTransactionStatus paymentStatus;
+
+    @NotNull
+    private String reason;
+
+}
