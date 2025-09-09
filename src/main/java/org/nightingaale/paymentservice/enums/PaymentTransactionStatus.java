95eee0fbe404b9233b452f9c37d460e2fb6efdd3
Nightingaale <shirounightingaleoff@gmail.com>
2025-09-09 22:32:06 +0300

    feature: add transaction status

---
+++ src/main/java/org/nightingaale/paymentservice/enums/PaymentTransactionStatus.java
@@ -0,0 +1,19 @@
+package org.nightingaale.paymentservice.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum PaymentTransactionStatus {
+    APPROVED,
+    REJECTED,
+    PROCESSING;
+
+    public static PaymentTransactionStatus fromString(String status) {
+        for (PaymentTransactionStatus value : PaymentTransactionStatus.values()) {
+            if (value.name().equalsIgnoreCase(status)) {
+                return value;
+            }
+        }
+        throw new IllegalArgumentException("Invalid PaymentTransactionStatus: " + status);
+    }
+}
