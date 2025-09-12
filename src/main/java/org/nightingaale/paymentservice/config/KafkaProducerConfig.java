de208b4ff5dccfd82c5391a9d2b9c155415ecb1b
Nightingaale <shirounightingaleoff@gmail.com>
2025-09-12 15:45:20 +0300

    feature: kafka producer

--- src/main/java/org/nightingaale/paymentservice/config/KafkaProducerConfig.java
+++ src/main/java/org/nightingaale/paymentservice/config/KafkaProducerConfig.java
@@ -2,8 +2,14 @@ package org.nightingaale.paymentservice.config;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.nightingaale.paymentservice.dto.PaymentTransactionDto;
+import org.nightingaale.paymentservice.dto.RefundTransactionDto;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
@@ -22,5 +28,25 @@ public class KafkaProducerConfig {
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
         return configProps;
+    }
+
+    @Bean
+    public ProducerFactory<String, PaymentTransactionDto> paymentTransactionProducerFactory() {
+        return new DefaultKafkaProducerFactory<>(baseConfig());
+    }
+
+    @Bean
+    public KafkaTemplate<String, PaymentTransactionDto> paymentTransactionKafkaTemplate() {
+        return new KafkaTemplate<>(paymentTransactionProducerFactory());
+    }
+
+    @Bean
+    public ProducerFactory<String, RefundTransactionDto> refundTransactionProducerFactory() {
+        return new DefaultKafkaProducerFactory<>(baseConfig());
+    }
+
+    @Bean
+    public KafkaTemplate<String, RefundTransactionDto> refundTransactionKafkaTemplate() {
+        return new KafkaTemplate<>(refundTransactionProducerFactory());
     }
 }
