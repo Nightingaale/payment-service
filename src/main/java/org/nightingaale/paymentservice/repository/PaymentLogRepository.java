46f2b20d74ef819df2cad92c3b82f39fd0248069
Nightingaale <shirounightingaleoff@gmail.com>
2025-10-12 22:58:10 +0300

    refactor: repo -> entity

--- src/main/java/org/nightingaale/paymentservice/repository/PaymentLogRepository.java
+++ src/main/java/org/nightingaale/paymentservice/repository/PaymentLogRepository.java
@@ -1,6 +1,7 @@
 package org.nightingaale.paymentservice.repository;
 
+import org.nightingaale.paymentservice.model.entity.PaymentLogEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PaymentLogRepository extends JpaRepository<PaymentLogRepository, Long> {
+public interface PaymentLogRepository extends JpaRepository<PaymentLogEntity, Long> {
 }
