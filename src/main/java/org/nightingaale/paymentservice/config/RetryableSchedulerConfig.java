397558ab14aa1731a3e9a00b871df44bc1d759d8
Nightingaale <shirounightingaleoff@gmail.com>
2025-10-23 21:16:03 +0300

    feature: add scheduler config

---
+++ src/main/java/org/nightingaale/paymentservice/config/RetryableSchedulerConfig.java
@@ -0,0 +1,9 @@
+package org.nightingaale.paymentservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class RetryableSchedulerConfig {
+}
