ef47d6999bbdaedf7f960a2f744d8f4c9047edc2
Nightingaale <shirounightingaleoff@gmail.com>
2025-11-02 20:30:09 +0300

    refactor: uuid -> string

--- src/main/java/org/nightingaale/paymentservice/repository/outbox/OutboxRepository.java
+++ src/main/java/org/nightingaale/paymentservice/repository/outbox/OutboxRepository.java
@@ -3,7 +3,5 @@ package org.nightingaale.paymentservice.repository.outbox;
 import org.nightingaale.paymentservice.model.entity.outbox.OutboxEventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
-public interface OutboxRepository extends JpaRepository<OutboxEventEntity, UUID> {
+public interface OutboxRepository extends JpaRepository<OutboxEventEntity, String> {
 }
