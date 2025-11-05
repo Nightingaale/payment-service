0173683455ae4b7ec3cced036ae4d6778e6b4241
Nightingaale <shirounightingaleoff@gmail.com>
2025-11-05 20:33:11 +0300

    refactor: remove pessimistic lock from models

--- src/main/java/org/nightingaale/paymentservice/repository/outbox/OutboxRepository.java
+++ src/main/java/org/nightingaale/paymentservice/repository/outbox/OutboxRepository.java
@@ -1,10 +1,7 @@
 package org.nightingaale.paymentservice.repository.outbox;
-
-import jakarta.persistence.LockModeType;
 import org.nightingaale.paymentservice.model.entity.outbox.OutboxEventEntity;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -14,7 +11,6 @@ import java.util.List;
 @Repository
 public interface OutboxRepository extends JpaRepository<OutboxEventEntity, String> {
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT o FROM OutboxEventEntity o " +
             "WHERE o.processed = false " +
             "AND o.createdAt <= :until " +
