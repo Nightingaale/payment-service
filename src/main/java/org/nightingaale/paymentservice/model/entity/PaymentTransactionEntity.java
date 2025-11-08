4ebc194d554bc977cd68709b83d943ef9e5566ae
Nightingaale <shirounightingaleoff@gmail.com>
2025-11-08 06:16:27 +0300

    refactor: remove unique key for user id

--- src/main/java/org/nightingaale/paymentservice/model/entity/PaymentTransactionEntity.java
+++ src/main/java/org/nightingaale/paymentservice/model/entity/PaymentTransactionEntity.java
@@ -32,7 +32,7 @@ public class PaymentTransactionEntity extends BaseEntity {
     private UUID paymentTransactionId;
 
     @NotNull
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String userId;
 
     @Convert(converter = PaymentTransactionStatusConverter.class)
