e43c78010f82792b5974ff4e094ebc58b5414eb5
Nightingaale <shirounightingaleoff@gmail.com>
2025-11-10 13:53:39 +0300

    refactor: return mappings

--- src/main/java/org/nightingaale/paymentservice/mapper/outbox/OutboxEventFactoryMapper.java
+++ src/main/java/org/nightingaale/paymentservice/mapper/outbox/OutboxEventFactoryMapper.java
@@ -1,6 +1,7 @@
 package org.nightingaale.paymentservice.mapper.outbox;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
 import org.nightingaale.paymentservice.model.entity.PaymentTransactionEntity;
@@ -8,5 +9,11 @@ import org.nightingaale.paymentservice.model.entity.outbox.OutboxEventEntity;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface OutboxEventFactoryMapper {
+    @Mapping(target = "aggregateType", constant = "PaymentTransaction")
+    @Mapping(target = "aggregateId", expression = "java(transactionEntity.getId().toString())")
+    @Mapping(target = "type", constant = "PaymentTransactionCreated")
+    @Mapping(target = "payload", expression = "java(\"{\\\"transactionId\\\":\\\"\" + transactionEntity.getId() + \"\\\"}\")")
+    @Mapping(target = "createdAt", expression = "java(java.time.Instant.now())")
+    @Mapping(target = "processed", constant = "false")
     OutboxEventEntity fromPaymentTransaction(PaymentTransactionEntity transactionEntity);
 }
