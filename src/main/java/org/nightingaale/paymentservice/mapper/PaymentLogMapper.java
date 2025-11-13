4debbfece271cc216bbc14b8dd5b3ac866c6e5f2
Nightingaale <shirounightingaleoff@gmail.com>
2025-11-13 15:25:03 +0300

    feature: add log saving

--- src/main/java/org/nightingaale/paymentservice/mapper/PaymentLogMapper.java
+++ src/main/java/org/nightingaale/paymentservice/mapper/PaymentLogMapper.java
@@ -1,15 +1,18 @@
 package org.nightingaale.paymentservice.mapper;
 
 import org.mapstruct.*;
-import org.nightingaale.paymentservice.model.dto.PaymentLogDto;
 import org.nightingaale.paymentservice.model.entity.PaymentLogEntity;
+import org.nightingaale.paymentservice.model.entity.PaymentTransactionEntity;
 
 @Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PaymentLogMapper {
-    PaymentLogEntity toEntity(PaymentLogDto paymentLongDto);
-
-    PaymentLogDto toDto(PaymentLogEntity paymentLogEntity);
-
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    PaymentLogEntity partialUpdate(PaymentLogDto paymentLongDto, @MappingTarget PaymentLogEntity paymentLogEntity);
+    @Mapping(target = "logId", ignore = true)
+    @Mapping(target = "refundTransaction", ignore = true)
+    @Mapping(target = "paymentTransaction", source = "transactionEntity")
+    @Mapping(target = "status", expression = "java(transactionEntity.getStatus())")
+    @Mapping(target = "provider", expression = "java(transactionEntity.getProvider())")
+    @Mapping(target = "methodType", expression = "java(transactionEntity.getMethodType())")
+    @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
+    @Mapping(target = "updatedAt", expression = "java(java.time.LocalDateTime.now())")
+    PaymentLogEntity toLog(PaymentTransactionEntity transactionEntity);
 }
--- src/main/java/org/nightingaale/paymentservice/service/PaymentService.java
+++ src/main/java/org/nightingaale/paymentservice/service/PaymentService.java
@@ -6,15 +6,18 @@ import org.nightingaale.paymentservice.client.UserServiceClient;
 import org.nightingaale.paymentservice.event.CreatePaymentTransactionRequest;
 import org.nightingaale.paymentservice.handler.api.PaymentMethodHandler;
 import org.nightingaale.paymentservice.handler.provider.PaymentProviderHandler;
+import org.nightingaale.paymentservice.mapper.PaymentLogMapper;
 import org.nightingaale.paymentservice.mapper.PaymentTransactionMapper;
 import org.nightingaale.paymentservice.mapper.PaymentTransactionRequestMapper;
 import org.nightingaale.paymentservice.mapper.outbox.OutboxEventFactoryMapper;
 import org.nightingaale.paymentservice.model.dto.PaymentMethodDto;
+import org.nightingaale.paymentservice.model.entity.PaymentLogEntity;
 import org.nightingaale.paymentservice.model.entity.PaymentTransactionEntity;
 import org.nightingaale.paymentservice.model.entity.outbox.OutboxEventEntity;
 import org.nightingaale.paymentservice.model.enums.PaymentMethodProvider;
 import org.nightingaale.paymentservice.model.enums.PaymentMethodType;
 import org.nightingaale.paymentservice.model.enums.PaymentTransactionStatus;
+import org.nightingaale.paymentservice.repository.PaymentLogRepository;
 import org.nightingaale.paymentservice.repository.PaymentTransactionRepository;
 import org.nightingaale.paymentservice.repository.outbox.OutboxRepository;
 import org.springframework.stereotype.Service;
@@ -35,6 +38,8 @@ public class PaymentService {
     private final UserServiceClient userServiceClient;
     private final OutboxEventFactoryMapper outboxEventFactoryMapper;
     private final OutboxRepository outboxRepository;
+    private final PaymentLogMapper paymentLogMapper;
+    private final PaymentLogRepository paymentLogRepository;
 
     @Transactional
     public void createPaymentTransaction(CreatePaymentTransactionRequest request) {
@@ -62,6 +67,9 @@ public class PaymentService {
             transactionEntity.setPaymentStatus(PaymentTransactionStatus.PROCESSING);
 
             paymentTransactionRepository.save(transactionEntity);
+
+            PaymentLogEntity paymentLog = paymentLogMapper.toLog(transactionEntity);
+            paymentLogRepository.save(paymentLog);
 
             OutboxEventEntity outbox = outboxEventFactoryMapper.fromPaymentTransaction(transactionEntity);
             outboxRepository.save(outbox);
