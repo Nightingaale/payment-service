7a6133d98fc70f92073e23f4cdc63e2580f3e5d4
Nightingaale <shirounightingaleoff@gmail.com>
2025-11-15 14:40:57 +0300

    refactor: change port

--- src/main/java/org/nightingaale/paymentservice/client/UserServiceClient.java
+++ src/main/java/org/nightingaale/paymentservice/client/UserServiceClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "user-service", url = "http://user-service:8083", configuration = FeignClientConfig.class)
+@FeignClient(name = "user-service", url = "http://user-service:8086", configuration = FeignClientConfig.class)
 public interface UserServiceClient {
     @RequestMapping(method = RequestMethod.POST, value = "/api/v1/users/send")
     CreateUserIdRequest sendId(@RequestBody CreatePaymentTransactionRequest request);
