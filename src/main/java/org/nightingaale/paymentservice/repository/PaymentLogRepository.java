14d0dfd51fe63a89b76cc9267d17c852287ea0d5
Nightingaale <shirounightingaleoff@gmail.com>
2025-11-22 14:31:05 +0300

    refactor: remove logging table

--- src/main/java/org/nightingaale/paymentservice/repository/PaymentLogRepository.java
+++
@@ -1,7 +0,0 @@
-package org.nightingaale.paymentservice.repository;
-
-import org.nightingaale.paymentservice.model.entity.PaymentLogEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentLogRepository extends JpaRepository<PaymentLogEntity, String> {
-}
